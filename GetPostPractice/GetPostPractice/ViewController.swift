9ff9a39cb4c1975eeb2d588e429eb763c2ae8371
AndreyPrez <aw06.1994@yandex.ru>
2022-05-05 13:51:24 +0300

    Get request added

--- GetPostPractice/GetPostPractice/ViewController.swift
+++ GetPostPractice/GetPostPractice/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            guard let response = response,
+            let data = data
+            else { return }
+            print(data)
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch  {
+                print(error)
+            }
+        }.resume()
     }
     @IBAction func postRequest(_ sender: Any) {
     }
