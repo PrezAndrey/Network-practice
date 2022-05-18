3b4f6908c9b1a7226f7fca834c1112ff50e8e960
AndreyPrez <aw06.1994@yandex.ru>
2022-05-18 22:00:34 +0300

    Alamofire response added

--- GetPostPractice/Networking/Network/AlamofireNetworkRequest.swift
+++ GetPostPractice/Networking/Network/AlamofireNetworkRequest.swift
@@ -15,9 +15,27 @@ class AlamofireNetworkRequest {
         
         guard let url = URL(string: url) else { return }
         
-        AF.request(url, method: .get).responseJSON { (response) in
+        AF.request(url, method: .get).validate().responseJSON { (response) in
             
-            print(response)
+            switch response.result {
+            case .success(let value):
+                print(value)
+            case .failure(let error):
+                print(error)
+            }
+            
+//            guard let statusCode = response.response?.statusCode else { return }
+            
+//            print("Status code:", statusCode)
+//
+//
+//            if (200..<300).contains(statusCode) {
+//                let value = response.value
+//                print("Value", value ?? nil)
+//            } else {
+//                let error = response.error
+//                print(error ?? "Error")
+//            }
         }
     }
 }
